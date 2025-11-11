4af07a4ad2d343215bda19b716130c86a165bd58
Giu Eminente <giulemi210@gmail.com>
2025-11-11 12:30:12 +0100

    testing api service

--- src/app/services/api.spec.ts
+++ src/app/services/api.spec.ts
@@ -1,20 +1,31 @@
 import { TestBed } from '@angular/core/testing';
 import { provideZonelessChangeDetection } from '@angular/core';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { Api } from './api';
 
 describe('Api', () => {
   let service: Api;
+  let url: string;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
-        provideZonelessChangeDetection()
+        provideZonelessChangeDetection(),
+        provideHttpClientTesting()
       ]
     });
+   
+    url = 'https://api.themoviedb.org/3/'
     service = TestBed.inject(Api);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+  it('should call get method', () => {
+    const spy = spyOn(service, 'getList').and.callThrough();
+    service.getList(url);
+    expect(spy).toHaveBeenCalled();
+
   });
 });
---
+++ src/app/services/mocks-api.spec.ts
@@ -0,0 +1,8 @@
+import type { TMDBresponse } from './api';
+
+export const mockTMDBresponse: TMDBresponse = {
+    page: 1,
+    results: [],
+    total_pages: 1,
+    total_results: 1,
+};
